a3181b250c65149772c002fb2c0beeb750ecd502
Fortifelx <fortifelx@gmail.com>
2017-11-04 22:19:43 +0200

    add script to header

--- portal/assets/js/script.js
+++ portal/assets/js/script.js
@@ -7,5 +7,29 @@
 		})
 })();
 (function(){
-	
+	var $header  = $(".header_wrapper");
+
+	var fixed = "header_fixed";
+	var active = "header_active";
+
+	window.onscroll = function() {
+				var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+				var firstPosition = document.documentElement.clientHeight;
+				if(scrolled > firstPosition/2) {
+					$header.addClass(fixed);
+				}
+				if(scrolled > firstPosition) {
+					$header.addClass(active);
+				} 
+				if(scrolled < firstPosition/2) {
+					$header.removeClass(active);
+				}
+				if(scrolled < 90) {
+					$header.removeClass(fixed);
+				}
+
+
+
+
+	}
 })();
