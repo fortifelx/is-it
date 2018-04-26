eb73cc6b0106b37b128fbe2d16c6db290052c4e3
Fortifelx <fortifelx@gmail.com>
2018-04-27 02:30:59 +0300

    add ckeditor and change product card structure

--- cabinet/js/script.js
+++ cabinet/js/script.js
@@ -1,6 +1,66 @@
-Vue.component('product-list', {
-    props: ['id','img','name','shop','options','rating','like','published'],
-    template: '<h1 class="test">{{ name }} {{ id }}</h1>'
+Vue.component('ckeditor', {
+        template: `<div class="ckeditor"><textarea :id="id" :value="value"></textarea></div>`,
+        props: {
+            value: {
+                type: String
+            },
+            id: {
+                type: String,
+                default: 'editor'
+            },
+            height: {
+                type: String,
+                default: '30vh',
+            },
+            toolbar: {
+                type: Array,
+                default: () => [
+                ['Undo','Redo'],
+                ['Bold','Italic','Strike'],
+                ['NumberedList','BulletedList'],
+                ['Cut','Copy','Paste'],
+            ]
+        },
+        language: {
+            type: String,
+            default: 'ru'
+        },
+        extraplugins: {
+            type: String,
+            default: ''
+        }
+    },
+    beforeUpdate () {
+    const ckeditorId = this.id
+    if (this.value !== CKEDITOR.instances[ckeditorId].getData()) {
+        CKEDITOR.instances[ckeditorId].setData(this.value)
+    }
+},
+mounted () {
+    const ckeditorId = this.id
+    console.log(this.value)
+    const ckeditorConfig = {
+        toolbar: this.toolbar,
+        language: this.language,
+        height: this.height,
+        extraPlugins: this.extraplugins
+    }
+    CKEDITOR.replace(ckeditorId, ckeditorConfig)
+    CKEDITOR.instances[ckeditorId].setData(this.value)
+    CKEDITOR.instances[ckeditorId].on('change', () => {
+        let ckeditorData = CKEDITOR.instances[ckeditorId].getData()
+        if (ckeditorData !== this.value) {
+        this.$emit('input', ckeditorData)
+    }
+})
+},
+destroyed () {
+    const ckeditorId = this.id
+    if (CKEDITOR.instances[ckeditorId]) {
+        CKEDITOR.instances[ckeditorId].destroy()
+    }
+}
+
 });
 var section = new Vue({
    el: "#section",
@@ -153,10 +213,8 @@ var section = new Vue({
             this.newOption = '';
        },
         removeOption: function(e){
-           console.log(e.target);
            var i = e.target.dataset.id;
            this.newProduct.options.splice(i, 1);
-            console.log(this.newProduct.options);
         }
     }
 });
